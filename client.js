1763a9682a4934a0ddb338e9e1c12a5f2a31dd23
Mr. Fleming <efleming@stpatrick.org>
2018-04-26 21:30:13 -0500

    initial upload

---
+++ client.js
@@ -0,0 +1,65 @@
+//imports code from other file
+const importTask = require("./task.js");
+const Task = new importTask();
+
+/*
+    If you want to create a task type....... Task.createTask()
+    If you want to create a list of task objects type......Task.Factory()
+*/
+
+
+//function should return a totalValue and totalTime variable wrapped in an object
+function totals(list){
+
+
+
+}
+
+//works the the same as above, but stops short at the kth number in the list
+function partialTotals(list,k){
+
+
+
+}
+
+//returns a sorted version of the task list from least to greatest according to its time variable
+function sortTime(list){
+
+
+}
+
+//returns a sorted version of the task list from least to greatest according to its value variable
+function sortValue(list){
+
+
+}
+
+//returns a sorted version of the task list from least to greatest according to its impact
+function sortImpact(list){
+
+
+}
+
+
+//this is the test code to see if your functions work
+function mainTest(n){
+    let taskList = Task.Factory(n);
+    //tests basic totals
+    console.log(totals(taskList));
+    console.log(partialTotals(taskList, n/4));
+
+    //tests to make sure the sorts are effective
+    //both should outperform the original
+    let minTime = sortTime(taskList);
+    console.log(partialTotals(minTime, n/4));
+
+    let minValue = sortValue(taskList);
+    let maxValue = minValue.reverse();
+    console.log(partialTotals(maxValue, n/4));
+
+    let minImpact = sortImpact(taskList);
+    let maxImpact = minImpact.reverse();
+    console.log(partialTotals(maxImpact, n/4));
+}
+
+mainTest(60);
---
+++ task.js
@@ -0,0 +1,28 @@
+//The code at the bottom is used to export this file as a module to be used in the client file.
+//The fist line sets up my export as well, ignore it.
+const TaskExport = function(){
+
+    //returns a task object, similar to pizza
+    function createTask(v,t){
+
+    }
+
+    //returns a random integer between 0 and n inclusive
+    function rInt(n){
+
+    }
+    //returns a list of task objects.
+    //You must reference the function above to create them each time
+    //make all times range from 0 to 15 and all values range from 0 to 100
+    function Factory(n){
+
+    }
+
+    //do not change anything below there!
+    return{
+        createTask,
+        Factory
+    }
+
+}
+module.exports = TaskExport;
